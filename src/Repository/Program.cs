189089bab5b5f4130d6f37f1c72271530e0eb51b
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-03-02 19:40:23 +0200

    Added Unity and runtime strategy resolution.

--- src/Repository/Program.cs
+++ src/Repository/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace Repository
+﻿using Microsoft.Practices.Unity;
+
+using Repository.Infrastructure;
+
+namespace Repository
 {
     public static class Program
     {
         public static void Main()
-        { 
+        {
+            var c = UnityConfig.Register(UnityConfig.Mock);
+            var facade = (Facade)c.Resolve(typeof(Facade));
+            facade.DoSomething();
+        }
+
+        private class Facade
+        {
+            private readonly IUnitOfWorkProvider unitOfWorkProvider;
+
+            public Facade(IUnitOfWorkProvider unitOfWorkProvider)
+            {
+                this.unitOfWorkProvider = unitOfWorkProvider;
+            }
+
+            public void DoSomething()
+            {
+                using (this.unitOfWorkProvider.Create("ro"))
+                {
+                }
+
+                using (var unitOfWork = this.unitOfWorkProvider.Create("rw"))
+                {
+                    unitOfWork.Commit();
+                }
+            }
         }
     }
 }
---
+++ src/Repository/UnityConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.Practices.Unity;
+
+using Repository.Infrastructure;
+
+namespace Repository
+{
+    public static class UnityConfig
+    {
+        public const string Mock = "Mock";
+
+        public static IUnityContainer Register(string strategy)
+        {
+            var mi = typeof(UnityConfig).GetMethod("Register" + strategy);
+            if (mi == null)
+            {
+                throw new ArgumentOutOfRangeException("strategy");
+            }
+
+            var c = new UnityContainer();
+            mi.Invoke(null, new object[] { c });
+            return c;
+        }
+
+        public static void RegisterMock(IUnityContainer c)
+        {
+            c.RegisterType<ContextAccessor>(new HierarchicalLifetimeManager());
+            c.RegisterType<IUnitOfWorkProvider, Mock.UnitOfWorkProvider>(new HierarchicalLifetimeManager());
+        }
+    }
+}
