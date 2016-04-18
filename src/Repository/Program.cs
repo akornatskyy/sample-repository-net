e808a90b2c4ad5d342de4bc08e4476d0be6f4732
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-04-18 20:21:40 +0300

    Updated use case to use membership repository.

--- src/Repository/Program.cs
+++ src/Repository/Program.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Practices.Unity;
+﻿using System.Threading.Tasks;
+
+using Microsoft.Practices.Unity;
 
 using Repository.Infrastructure;
+using Repository.Interface;
 
 namespace Repository
 {
@@ -8,30 +11,64 @@ namespace Repository
     {
         public static void Main()
         {
+            UseCase("andriy.kornatskyy@live.com");
+        }
+
+        private static void UseCase(string email)
+        {
             var c = UnityConfig.Register(UnityConfig.Mock);
             var facade = (Facade)c.Resolve(typeof(Facade));
-            facade.DoSomething();
+
+            // Avoid concurrency for unit of work, underlying db connection
+            // does not support MultipleActiveResultSets.
+            // Task.WaitAll(facade.HasUser(email), facade.EnsureUser(email));
+            Task.WaitAll(facade.HasUser(email));
+            Task.WaitAll(facade.EnsureUser(email));
         }
 
         private class Facade
         {
             private readonly IUnitOfWorkProvider unitOfWorkProvider;
+            private readonly IMembershipRepository membershipRepository;
 
-            public Facade(IUnitOfWorkProvider unitOfWorkProvider)
+            public Facade(IUnitOfWorkProvider unitOfWorkProvider, IMembershipRepository membershipRepository)
             {
                 this.unitOfWorkProvider = unitOfWorkProvider;
+                this.membershipRepository = membershipRepository;
             }
 
-            public void DoSomething()
+            public async Task<bool> HasUser(string email)
             {
-                using (this.unitOfWorkProvider.Create("ro"))
+                int id;
+                using (await this.unitOfWorkProvider.Create("ro"))
                 {
+                    id = await this.membershipRepository.HasUser(email);
                 }
 
-                using (var unitOfWork = this.unitOfWorkProvider.Create("rw"))
+                return id > 0;
+            }
+
+            public async Task<int> EnsureUser(string email)
+            {
+                int id;
+                using (var unitOfWork = await this.unitOfWorkProvider.Create("rw"))
                 {
+                    id = await this.membershipRepository.HasUser(email);
+                    if (id > 0)
+                    {
+                        return id;
+                    }
+
+                    id = await this.membershipRepository.AddUser(email);
+                    if (id <= 0)
+                    {
+                        return 0;
+                    }
+
                     unitOfWork.Commit();
                 }
+
+                return id;
             }
         }
     }
