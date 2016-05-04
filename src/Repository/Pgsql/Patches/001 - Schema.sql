2cc6b116d4add6d9ed82ec51706d490e10ea6538
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-05-04 20:12:46 +0300

    Updated postgresql schema with access schema, tables and sequences default privileges.

--- src/Repository/Pgsql/Patches/001 - Schema.sql
+++ src/Repository/Pgsql/Patches/001 - Schema.sql
@@ -1,5 +1,7 @@
 ﻿CREATE SCHEMA membership;
 
+-- ROLES
+
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_user WHERE usename = 'sample-ro') THEN
@@ -10,3 +12,33 @@ BEGIN
     END IF;
 END
 $$;
+
+-- ACCESS SCHEMA
+
+REVOKE ALL     ON SCHEMA public FROM public;
+GRANT  USAGE   ON SCHEMA public TO "sample-ro";
+GRANT  USAGE   ON SCHEMA public TO "sample-rw";
+
+REVOKE ALL     ON SCHEMA membership FROM public;
+GRANT  USAGE   ON SCHEMA membership TO "sample-ro";
+GRANT  USAGE   ON SCHEMA membership TO "sample-rw";
+
+-- ACCESS TABLES
+
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM public;
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "sample-ro";
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM "sample-rw";
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA membership
+    GRANT SELECT ON TABLES
+    TO "sample-ro";
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA membership
+    GRANT INSERT, SELECT, UPDATE, DELETE ON TABLES
+    TO "sample-rw";
+
+-- ACCESS SEQUENCES
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA membership
+    GRANT USAGE ON SEQUENCES
+    TO "sample-rw";
