66836ec19c42529a19c4585d1784613644dc32cb
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-05-08 10:23:55 +0300

    Added pgsql functions for AddUser and HasUser.

---
+++ src/Repository/Pgsql/Programmability/AddUser.sql
@@ -0,0 +1,36 @@
+﻿CREATE OR REPLACE FUNCTION membership.add_user
+(
+	email VARCHAR
+)
+RETURNS TABLE (Id INTEGER)
+AS $$
+	WITH x AS 
+	(
+		INSERT INTO membership.user
+			(created_on, email)
+		VALUES 
+			(DEFAULT, LOWER($1))
+		RETURNING
+			id, created_on
+	),
+	a AS 
+	(
+		INSERT INTO membership.address
+		(
+			user_id, modified_on
+		)
+		SELECT
+			id, created_on
+		FROM x
+	)
+	INSERT INTO membership.person
+	(
+		user_id, modified_on
+	)
+	SELECT
+		id, created_on
+	FROM x
+	RETURNING user_id;
+$$ LANGUAGE SQL;
+
+GRANT EXECUTE ON FUNCTION membership.add_user(VARCHAR) TO "sample-rw";
---
+++ src/Repository/Pgsql/Programmability/HasUser.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE FUNCTION membership.has_user
+(
+	email VARCHAR
+)
+RETURNS TABLE(id INTEGER)
+AS $$
+	SELECT
+		id id
+	FROM membership.user
+	WHERE
+		email = $1
+	LIMIT 1
+$$ LANGUAGE SQL;
+
+GRANT EXECUTE ON FUNCTION membership.has_user(VARCHAR) TO "sample-ro";
