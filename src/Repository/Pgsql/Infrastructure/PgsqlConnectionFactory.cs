c16813b2fedf586f6f925130fecaab986fab37b0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-05-09 11:52:12 +0300

    Introduced pgsql repository.

---
+++ src/Repository/Pgsql/Infrastructure/PgsqlConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+using Npgsql;
+
+using Repository.Infrastructure.Data;
+
+namespace Repository.Pgsql.Infrastructure
+{
+    public sealed class PgsqlConnectionFactory : IDbConnectionFactory
+    {
+        public async Task<IDbConnection> Create(string connectionString)
+        {
+            var c = new NpgsqlConnection(connectionString);
+            await c.OpenAsync();
+            return c;
+        }
+    }
+}
---
+++ src/Repository/Pgsql/MembershipRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Repository.Infrastructure;
+using Repository.Infrastructure.Data;
+using Repository.Interface;
+
+namespace Repository.Pgsql
+{
+    public sealed class MembershipRepository : IMembershipRepository
+    {
+        private readonly ContextAccessor accessor;
+
+        public MembershipRepository(ContextAccessor accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        private DbContext Context
+        {
+            get
+            {
+                var ctx = this.accessor.Context as DbContext;
+                Debug.Assert(ctx != null, "ctx != null");
+                return ctx;
+            }
+        }
+
+        public Task<int> HasUser(string email)
+        {
+            return this.Context.SpFirstOrDefaultAsync<int>("membership.has_user", new { email });
+        }
+
+        public Task<int> AddUser(string email)
+        {
+            return this.Context.SpFirstOrDefaultAsync<int>("membership.add_user", new { email });
+        }
+    }
+}
